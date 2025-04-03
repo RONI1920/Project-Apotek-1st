df70a4a9f67224ceb7e51b596ebe9b3bb9918664
Roni Hidayat <ronihidayat66.99@gmail.com>
2025-04-04 01:37:21 +0700

    tambah css

--- admin.php
+++ admin.php
@@ -26,24 +26,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <title>Tambah Obat</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <h2>Tambah Obat</h2>
+    <div class="form-container">
     <form method="POST">
+        <div class="form-group">
         <label>Nama Obat:</label>
         <input type="text" name="nama" required>
+        </div>
         <br>
+        <div class="form-group">
         <label>Harga:</label>
         <input type="number" name="harga" required>
+        </div>
         <br>
+        <div class="form-group">
         <label>Stok:</label>
         <input type="number" name="stok" required>
+        </div>
         <br>
+        <div class="form-group">
         <label>Deskripsi:</label>
         <textarea name="deskripsi"></textarea>
+        </div>
         <br>
         <button type="submit">Simpan</button>
     </form>
+    </div>
     <a href="index.php">Kembali</a>
 </body>
 </html>
